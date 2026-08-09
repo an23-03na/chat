beea2bb79b4b38aae30ff1e3e196c0c613c94bab
an23-03na <annachyan2303@gmail.com>
2026-08-09 21:55:03 +0400

    update

--- apps/backend/src/lib/socket.ts
+++ apps/backend/src/lib/socket.ts
@@ -4,7 +4,7 @@ import express from "express";
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: { origin: "http://localhost:3000" },
+  cors: { origin: process.env.FRONTEND_URL },
 });
 
 const userSocket: Record<string, string> = {};
