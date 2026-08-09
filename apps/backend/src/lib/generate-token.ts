5d9689bff40fd1c5a7051db09bc3fbe49ed158df
an23-03na <annachyan2303@gmail.com>
2026-08-09 22:54:09 +0400

    update

--- apps/backend/src/lib/generate-token.ts
+++ apps/backend/src/lib/generate-token.ts
@@ -8,8 +8,8 @@ export const generateToken = (userId: string, res: Response) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: "strict",
-    secure: process.env.NODE_ENV !== "development",
+    sameSite: "none",
+    secure: true,
   });
   return token;
 };
