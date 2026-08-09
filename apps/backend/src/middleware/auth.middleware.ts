a997e56f1e20c11470982d4fe92281dda0f95902
an23-03na <annachyan2303@gmail.com>
2026-08-09 22:26:03 +0400

    update

--- apps/backend/src/middleware/auth.middleware.ts
+++ apps/backend/src/middleware/auth.middleware.ts
@@ -11,6 +11,42 @@ declare global {
   }
 }
 
+// export const protectRoute = async (
+//   req: Request,
+//   res: Response,
+//   next: NextFunction,
+// ) => {
+//   try {
+//     const token = req.cookies.jwt;
+
+//     if (!token) {
+//       return res.status(401).json({ message: "not authorized token" });
+//     }
+
+//     const decoded = jwt.verify(token, process.env.JWT_SECRET || "") as {
+//       userId: string;
+//     };
+
+//     if (!decoded) {
+//       return res.status(401).json({ message: "not authorized decoded" });
+//     }
+
+//     const user = await prisma.user.findUnique({
+//       where: { id: decoded.userId },
+//     });
+
+//     if (!user) {
+//       return res.status(401).json({ message: "not authorized user" });
+//     }
+
+//     req.user = user;
+
+//     next();
+//   } catch (error) {
+//     console.log("Internal Server Error protectRoute");
+//     return res.status(500).json({ message: "Internal Server Error" });
+//   }
+// };
 export const protectRoute = async (
   req: Request,
   res: Response,
@@ -27,10 +63,6 @@ export const protectRoute = async (
       userId: string;
     };
 
-    if (!decoded) {
-      return res.status(401).json({ message: "not authorized decoded" });
-    }
-
     const user = await prisma.user.findUnique({
       where: { id: decoded.userId },
     });
@@ -40,10 +72,9 @@ export const protectRoute = async (
     }
 
     req.user = user;
-
     next();
   } catch (error) {
-    console.log("Internal Server Error protectRoute");
-    return res.status(500).json({ message: "Internal Server Error" });
+    console.log("JWT ERROR:", error); // <-- вот это важно
+    return res.status(401).json({ message: "invalid token" });
   }
 };
