a6a7b7070150abafe0c382002c74b0acd51fe0d6
an23-03na <annachyan2303@gmail.com>
2026-08-09 21:36:52 +0400

    update

--- apps/backend/prisma.config.ts
+++ apps/backend/prisma.config.ts
@@ -4,6 +4,6 @@ import { defineConfig, env } from "prisma/config";
 export default defineConfig({
   schema: "src/models",
   datasource: {
-    url: env("DIRECT_URL"),
+    url: env("DATABASE_URL"),
   },
 });
