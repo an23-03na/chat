3c0171a4d7b66cf1078f09314a0a64a223818c91
an23-03na <annachyan2303@gmail.com>
2026-08-09 23:14:11 +0400

    update

--- apps/frontend/src/proxy.ts
+++ apps/frontend/src/proxy.ts
@@ -4,6 +4,10 @@ const publicRoutes = ["/login", "/signup"];
 
 export function proxy(req: NextRequest) {
   const token = req.cookies.get("jwt")?.value;
+
+  console.log("PROXY JWT:", !!token);
+  console.log("PROXY URL:", req.nextUrl.pathname);
+
   const isPublicRoute = publicRoutes.includes(req.nextUrl.pathname);
 
   if (!token && !isPublicRoute) {
