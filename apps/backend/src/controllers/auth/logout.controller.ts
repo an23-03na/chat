61d2182204e20f3233072077f6c7b67587a8d956
an23-03na <annachyan2303@gmail.com>
2026-08-12 20:43:11 +0400

    update

--- apps/backend/src/controllers/auth/logout.controller.ts
+++ apps/backend/src/controllers/auth/logout.controller.ts
@@ -2,7 +2,13 @@ import { Request, Response } from "express";
 
 export const logout = async (req: Request, res: Response) => {
   try {
-    res.clearCookie("jwt");
+    const isProd = process.env.NODE_ENV !== "development";
+    res.clearCookie("jwt", {
+      httpOnly: true,
+      sameSite: isProd ? "none" : "lax",
+      secure: isProd,
+      domain: isProd ? ".mk-flower.am" : undefined,
+    });
     return res.status(200).json({ message: "success logout" });
   } catch (error) {
     console.log("Server Error logout ", error);
--- apps/backend/src/lib/generate-token.ts
+++ apps/backend/src/lib/generate-token.ts
@@ -5,11 +5,14 @@ export const generateToken = (userId: string, res: Response) => {
     expiresIn: "7d",
   });
 
+  const isProd = process.env.NODE_ENV !== "development";
+
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: "none",
-    secure: true,
+    sameSite: isProd ? "none" : "lax",
+    secure: isProd,
+    domain: isProd ? ".mk-flower.am" : undefined,
   });
   return token;
 };
