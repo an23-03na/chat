3fafc185fa60cd95425a55d29233daee57e7200f
an23-03na <annachyan2303@gmail.com>
2026-08-12 20:01:54 +0400

    update

--- apps/frontend/src/proxy.ts
+++ apps/frontend/src/proxy.ts
@@ -1,6 +1,16 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export function proxy() {
+const publicRoutes = ["/login", "/signup"];
+
+export function proxy(req: NextRequest) {
+  const token = req.cookies.get("jwt")?.value;
+  
+  const isPublicRoute = publicRoutes.includes(req.nextUrl.pathname);
+
+  if (!token && !isPublicRoute) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
   return NextResponse.next();
 }
 export const config = {
